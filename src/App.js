c87747a870c28868f1944986ffbbac62bec8db9b
Prabin Kumar Sahu <78588455+Prabin-Kumar-Sahu@users.noreply.github.com>
2023-08-20 20:13:27 +0530

    commit 2

--- src/App.js
+++ src/App.js
@@ -13,76 +13,113 @@ import {
   onAuthStateChanged,
   getAuth,
   GoogleAuthProvider,
-  signInWithPopup,signOut
+  signInWithPopup,
+  signOut,
 } from "firebase/auth";
-import {getFirestore,addDoc, collection, serverTimestamp} from "firebase/firestore"
+import {
+  getFirestore,
+  addDoc,
+  collection,
+  serverTimestamp,
+  onSnapshot,
+  query,orderBy
+} from "firebase/firestore";
 
 
 const auth = getAuth(app);
-const db=getFirestore(app);
-
+const db = getFirestore(app);
 
 const loginHandler = () => {
   const provider = new GoogleAuthProvider();
   signInWithPopup(auth, provider);
 };
 
-
-const logoutHandler=()=> signOut(auth)
-
+const logoutHandler = () => signOut(auth);
 
 function App() {
+  const q=query(collection(db,"Messages"),orderBy("createdAt","asc"))
   const [user, setUser] = useState(false);
-  const [message,setMessage]=useState("")
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
 
+  const submitHandler = async (e) => {
+    e.preventDefault();
 
-  const submitHandler=async(e)=>
-{
-  e.preventDefault()
-
-  try {
-    await addDoc(collection(db,"Messages"),{
-      text:message,
-      uid:user.uid,
-      uri:user.photoURL,
-      createdAt:serverTimestamp()
-      
-    })
-    setMessage("")
-  } catch (error) {
-    alert(error)
-    
-  }
-}
-
-  useEffect(()=>
-  {
-   const unsubscribe= onAuthStateChanged(auth,(data)=>
-    {
-     setUser(data);
-    })
-    return ()=>{
-      unsubscribe()
+    try {
+      await addDoc(collection(db, "Messages"), {
+        text: message,
+        uid: user.uid,
+        uri: user.photoURL,
+        createdAt: serverTimestamp(),
+      });
+      setMessage("");
+    } catch (error) {
+      alert(error);
     }
-  })
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (data) => {
+      setUser(data);
+    });
+
+    const unsubscribeForMessage = onSnapshot(
+      q,
+      (snap) => {
+        setMessages(
+          snap.docs.map((item) => {
+            const id = item.id;
+            return { id, ...item.data() };
+          })
+        );
+      }
+    );
+    return () => {
+      unsubscribe();
+      unsubscribeForMessage();
+        };
+  });
 
   return (
     <Box bg={"black"}>
-      { user ? (
+      {user ? (
         <Container h={"100vh"} bg={"white"}>
-          <VStack bg={"telegram.400"} padding={"4"} h="full">
+          <VStack bg={"white"} padding={"4"} h="full">
             <Button onClick={logoutHandler} colorScheme={"red"} w={"full"}>
               LogOut
             </Button>
+            {/* <VStack h="full" w={"full"} overflowY={"auto"}>
+              {messages.map((item) => {
+                <Message
+                  key={item.id}
+                  user={item.uri === user.uid ? "me" : "other"}
+                  text={item.text}
+                  uri={item.uri}
+                />
+              })}
+              
+            </VStack> */}
             <VStack h="full" w={"full"} overflowY={"auto"}>
-              <Message text={"sample"} />
-              <Message user="me" text={"sample"} />
-              <Message text={"sample"} />
-              <Message user="me" text={"sample"} />
+              {messages.map((item) => (
+                <Message
+                  key={item.id}
+                  user={item.uid === user.uid ? "me" : "other"}
+                  text={item.text}
+                  uri={item.uri}
+                />
+              ))}
             </VStack>
-            <form   onSubmit={submitHandler} style={{ width: "100%", backgroundColor: "white" }}>
+
+            <form
+              onSubmit={submitHandler}
+              style={{ width: "100%", backgroundColor: "white" }}
+            >
               <HStack>
-                <Input  value ={message} onChange={(e)=>setMessage(e.target.value)} placeholder="enter your message" />
+                <Input
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                  placeholder="enter your message"
+                />
                 <Button colorScheme={"green"} type="submit">
                   Send
                 </Button>
